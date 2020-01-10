3ca5f53d5ab9a435d253dd3ea861974820f07bbb
Synneks <Deniseneotescu@gmail.com>
2020-01-10 18:13:34 +0200

    created service for blocked-users-table and moved services in different folder

---
+++ src/app/service/blocked-users-table/blocked-users-table.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { BlockedUsersTableService } from './blocked-users-table.service';
+
+describe('BlockedUsersTableService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: BlockedUsersTableService = TestBed.get(BlockedUsersTableService);
+    expect(service).toBeTruthy();
+  });
+});
